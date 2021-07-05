64b90b14611b240016551111fa78ce3f17dc210c
divyahemnani <81250732+divyahemnani@users.noreply.github.com>
2021-07-05 23:00:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const API_KEY = 'api_key=5a159ba656f74a280ac7057ef8945790';
 const BASE_URL ='https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/movie?' + API_KEY;
+
 
 const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
 
 getMovies(API_URL);
 
@@ -15,26 +19,30 @@ function getMovies(url){
     })
 }
 
+
 function showMovies(data) {
     main.innerHTML='';
+
     
     data.forEach(movie => {
-        const title = movie;
-        const poster_path = movie;
+        const {title, poster_path, vote_average, overview} = movie;
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
-        movieEl.innerHTML='<img src="${IMG_URL + movie.poster_path}" alt="${movie.title}">\
-        <div class="movie-info">\
-        <h3>\
-        ${title}\
-        </h3>\
-        <span class="${getColor(vote_average)}">${vote_average}</span>\
-        </div>\
-        <div class="overview">\
-        <h3>Overview</h3>\
-        ${overview}\
-        </div>';
+        movieEl.innerHTML=` 
+        <img src="${IMG_URL + poster_path}" alt="${title}">
+        <div class="movie-info">
+            <h3>
+                ${title}
+            </h3>
+            <span class="${getColor(vote_average)}">${vote_average}</span>
+
+        </div>
+        <div class="overview">
+            <h3>overview</h3>
+            ${overview}
+        </div>
         
+        `
         
         main.appendChild(movieEl);
         
@@ -51,3 +59,15 @@ function getColor(vote){
         return 'red'
     }
 }
+
+form.addEventListener('submit' , (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if(searchTerm){
+       getMovies(searchURL + '&query=' + searchTerm)
+    }else{
+        getMovies(API_URL);
+    }
+})
